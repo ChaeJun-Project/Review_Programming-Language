52c4c28f29a8f8ea167c3a7ec288af6d397233fb
chaezun <39609369+ChaeJun-Project@users.noreply.github.com>
2021-09-01 21:55:39 +0900

    C/C++ 기초 복습(21/9/1)
    전체 내용 복습

--- Effective C++/Effective C++.cpp
+++ Effective C++/Effective C++.cpp
@@ -3,14 +3,7 @@
 
 int main()
 {
-    int* pt = DynamicAllocate_cplusplus<int>(100);
-	pt[50] = 100;
-
-	std::cout<< pt[50]<<std::endl;
-
-	DeleteDynamicAllocate_cplusplus<int>(pt);
-
-	std::cout << pt[50];
+   
 
 	return 0;
 }
